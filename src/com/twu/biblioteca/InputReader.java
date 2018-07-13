d62d9a23aabd001a07d455192000d2f84e24bf43
euadelgado <albadelgadoc@gmail.com>
2018-07-13 12:11:48 +0200

    added exception when user does not input a number

--- src/com/twu/biblioteca/InputReader.java
+++ src/com/twu/biblioteca/InputReader.java
@@ -1,14 +1,20 @@
 package com.twu.biblioteca;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class InputReader {
 
     int getUserNumber() {
         Scanner scanner = new Scanner(System.in);
-        int userNumber = scanner.nextInt();
-        if (isOutOfBounds(userNumber)) return -1;
-        else return userNumber;
+        try {
+            int userNumber = scanner.nextInt();
+            if (isOutOfBounds(userNumber)) return -1;
+            else return userNumber;
+        } catch (InputMismatchException e) {
+            System.out.println("That was not a number!");
+        }
+        return -1;
     }
 
     private boolean isOutOfBounds(int userNumber) {
